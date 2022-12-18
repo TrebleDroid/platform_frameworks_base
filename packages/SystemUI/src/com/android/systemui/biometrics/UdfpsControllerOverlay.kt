831b77f8f4220b92259983ab0dcd6c9d8b10b4db
Pierre-Hugues Husson <phh@phh.me>
2022-12-18 18:20:40 -0500

    FOD support for Asus ZF8 and Samsung devices
    Thanks Asus for providing a free device to make this support
    And thanks @davigamer987 for donating enough to get a Samsung FOD device
    to make this

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -102,7 +102,9 @@ class UdfpsControllerOverlay(
         gravity = android.view.Gravity.TOP or android.view.Gravity.LEFT
         layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
         flags = (Utils.FINGERPRINT_OVERLAY_LAYOUT_PARAM_FLAGS or
-          WindowManager.LayoutParams.FLAG_SPLIT_TOUCH)
+          WindowManager.LayoutParams.FLAG_SPLIT_TOUCH) or
+          WindowManager.LayoutParams.FLAG_DIM_BEHIND
+        dimAmount = 0.0f
         privateFlags = WindowManager.LayoutParams.PRIVATE_FLAG_TRUSTED_OVERLAY
         // Avoid announcing window title.
         accessibilityTitle = " "
@@ -153,6 +155,9 @@ class UdfpsControllerOverlay(
 
                     windowManager.addView(this, coreLayoutParams.updateDimensions(animation))
                     touchExplorationEnabled = accessibilityManager.isTouchExplorationEnabled
+                    dimUpdate = {
+                        windowManager.updateViewLayout(this, coreLayoutParams.updateDimensions(animation).apply { dimAmount = it })
+                    }
                     overlayTouchListener = TouchExplorationStateChangeListener {
                         if (accessibilityManager.isTouchExplorationEnabled) {
                             setOnHoverListener { v, event -> onTouch(v, event, true) }
