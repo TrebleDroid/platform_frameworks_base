52b9f87be5545b05bdac641fc2646f89a1bbce7a
Pierre-Hugues Husson <phh@phh.me>
2023-01-03 17:59:00 -0500

    Always add HIDL fingerprint config (Galaxy A53 is missing it on A12 vendor) -- but first enumerate AIDL

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/FingerprintService.java
@@ -899,8 +899,9 @@ public class FingerprintService extends SystemService {
             final Handler handler = new Handler(thread.getLooper());
 
             handler.post(() -> {
-                addHidlProviders(hidlSensors);
                 addAidlProviders();
+                if(mServiceProviders.isEmpty())
+                    addHidlProviders(hidlSensors);
 
                 final IBiometricService biometricService = IBiometricService.Stub.asInterface(
                         ServiceManager.getService(Context.BIOMETRIC_SERVICE));
