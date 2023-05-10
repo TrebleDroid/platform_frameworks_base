TrebleDroid/platform_frameworks_base

commit e27e0e177fe88584a280e0f7f624c249623efea1

Dynamically resize boot animation to match screen size
Change-Id: I54e49fc6b8c670103852e212d1416e27ff976205

cmds/bootanimation/BootAnimation.cpp
@@ -583,6 +583,26 @@ status_t BootAnimation::readyToRun() {
     mFlingerSurface = s;
     mTargetInset = -1;
 
+    SLOGE("Got screen size %d, animation size %d", mWidth, mAnimation->width);
+    int origWidth = mAnimation->width;
+    if ( mAnimation->width*2 < mWidth ) {
+	    SLOGE("Making animation bigger");
+	    mAnimation->width *= 2;
+	    mAnimation->height *= 2;
+    } else if ( mWidth < mAnimation->width ) {
+	    SLOGE("Making animation smaller");
+	    mAnimation->width /= 2;
+	    mAnimation->height /= 2;
+    }
+    for (Animation::Part& part : mAnimation->parts) {
+	    for(auto& frame: part.frames) {
+		    if(frame.trimWidth == origWidth && frame.trimX == 0 && frame.trimY == 0) {
+			    frame.trimWidth = mAnimation->width;
+			    frame.trimHeight = mAnimation->height;
+		    }
+	    }
+    }
+
     // Rotate the boot animation according to the value specified in the sysprop
     // ro.bootanim.set_orientation_<display_id>. Four values are supported: ORIENTATION_0,
     // ORIENTATION_90, ORIENTATION_180 and ORIENTATION_270.

commit 1a4a531bc366dcba9f07920e52bacf68549cda97

bootanimation: Fix bootanimation when using mask+shine rather than animation
This was broken by the commit that automatically resizes animations

Fixes: e27e0e177fe88584a280e0f7f624c249623efea1

cmds/bootanimation/BootAnimation.cpp
@@ -583,24 +583,26 @@ status_t BootAnimation::readyToRun() {
     mFlingerSurface = s;
     mTargetInset = -1;
 
-    SLOGE("Got screen size %d, animation size %d", mWidth, mAnimation->width);
-    int origWidth = mAnimation->width;
-    if ( mAnimation->width*2 < mWidth ) {
-	    SLOGE("Making animation bigger");
-	    mAnimation->width *= 2;
-	    mAnimation->height *= 2;
-    } else if ( mWidth < mAnimation->width ) {
-	    SLOGE("Making animation smaller");
-	    mAnimation->width /= 2;
-	    mAnimation->height /= 2;
-    }
-    for (Animation::Part& part : mAnimation->parts) {
-	    for(auto& frame: part.frames) {
-		    if(frame.trimWidth == origWidth && frame.trimX == 0 && frame.trimY == 0) {
-			    frame.trimWidth = mAnimation->width;
-			    frame.trimHeight = mAnimation->height;
-		    }
-	    }
+    if ( mAnimation != nullptr ) {
+        SLOGE("Got screen size %d, animation size %d", mWidth, mAnimation->width);
+        int origWidth = mAnimation->width;
+        if ( mAnimation->width*2 < mWidth ) {
+            SLOGE("Making animation bigger");
+            mAnimation->width *= 2;
+            mAnimation->height *= 2;
+        } else if ( mWidth < mAnimation->width ) {
+            SLOGE("Making animation smaller");
+            mAnimation->width /= 2;
+            mAnimation->height /= 2;
+        }
+        for (Animation::Part& part : mAnimation->parts) {
+            for(auto& frame: part.frames) {
+                if(frame.trimWidth == origWidth && frame.trimX == 0 && frame.trimY == 0) {
+                    frame.trimWidth = mAnimation->width;
+                    frame.trimHeight = mAnimation->height;
+                }
+            }
+        }
     }
 
     // Rotate the boot animation according to the value specified in the sysprop