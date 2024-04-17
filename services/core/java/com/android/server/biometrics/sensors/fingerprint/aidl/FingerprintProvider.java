45770ed87625015255b52ada38a4058a837f55ad
Pierre-Hugues Husson <phh@phh.me>
2024-04-17 04:49:18 -0400

    Don't crash system_server if we fail to parse dynamic udfps position

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
@@ -214,13 +214,17 @@ android.util.Log.e("PHH-Enroll", "Poping AIDL fp provider");
                 android.util.Log.e("PHH-Enroll", " - " + loc.sensorLocationX + ", " + loc.sensorLocationY + ", " +loc.sensorRadius + ", disp =" + loc.display + ", shape " + loc.sensorShape);
             }
             if (locations.length == 1 && locations[0].sensorLocationX == 0) {
-                int[] otherValues = com.android.server.biometrics.AuthService.dynamicUdfpsProps(context);
-                if (otherValues.length > 0) {
-                    SensorLocation loc = new SensorLocation();
-                    loc.sensorLocationX = otherValues[0];
-                    loc.sensorLocationY = otherValues[1];
-                    loc.sensorRadius = otherValues[2];
-                    locations[0] = loc;
+                try {
+                    int[] otherValues = com.android.server.biometrics.AuthService.dynamicUdfpsProps(context);
+                    if (otherValues.length > 0) {
+                        SensorLocation loc = new SensorLocation();
+                        loc.sensorLocationX = otherValues[0];
+                        loc.sensorLocationY = otherValues[1];
+                        loc.sensorRadius = otherValues[2];
+                        locations[0] = loc;
+                    }
+                } catch(Throwable t) {
+                    android.util.Log.e("PHH-Enroll", "FingerprintProvider dynamicUdfpsProps", t);
                 }
             }
 
